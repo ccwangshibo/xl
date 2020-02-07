d57bba5a6c766dbfe7616ae6a6641696ca8ab681
Lail <ccwangshibo@163.com>
2020-02-07 22:41:05 +0800

    v:2.07

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="home">
     <div v-if="!showLoading">
+      <!--头部导航-->
       <Header></Header>
-      <Swiper :swiper_list="swiper_list"></Swiper> 
+      <!--轮播图-->
+      <Swiper :swiper_list="swiper_list"></Swiper>
+      <!--中间nav分类导航-->
+      <Nav :nav_list="nav_list"></Nav>
     </div>
     <van-loading v-else type="spinner" color="#75a342" style="position:absolute;left:50%;top:40%;transform:translate(-50%);">小赖正在拼命加载中...</van-loading>
   </div>
@@ -11,6 +15,7 @@
 <script>
 import Header from './components/header/Header'
 import Swiper from './components/swiper/Swiper'
+import Nav from './components/nav/Nav'
 import {getHomeData} from './../../service/api/index'
 export default {
   name: 'Home',
@@ -18,19 +23,26 @@ export default {
     return {
       // 首页轮播图数据
       swiper_list:[],
+      // nav导航数据
+      nav_list:[],
       // 是否显示加载过程
       showLoading:true,
     }
   },
   components:{
     Header,
-    Swiper
+    Swiper,
+    Nav
   },
   created(){
     // 接收请求
     getHomeData().then((response)=>{
       if(response.success){
+        // 轮播图数据
         this.swiper_list=response.data.list[0].icon_list;
+        // nav导航数据
+        this.nav_list=response.data.list[2].icon_list;
+        console.log(this.nav_list)
         // 数据请求完成,隐藏加载页面
         this.showLoading=false;
       }
---
+++ src/views/home/components/nav/Nav.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="wrapper">
+    <div class="itemWrapper" v-for="(nav,i) of nav_list" :key="i">
+      <div class="imageWrapper">
+        <img class="image" :src="nav.icon_url" :alt="nav.cid">
+      </div>
+      <div class="titleWrapper">
+        <div class="title">{{nav.name}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Nav",
+    props:{
+      nav_list:Array
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .wrapper{
+    padding: 0.9375rem 0;
+    background: #fff;
+  }
+  .wrapper::after{
+    content: "";
+    display: block;
+    clear: both;
+  }
+  .itemWrapper{
+    float: left;
+    width: 20%;
+    padding-top: 0.3rem;
+  }
+  .imageWrapper{
+    overflow: hidden;
+    text-align: center;
+  }
+  .image{
+    width: 58%;
+    height: auto;
+  }
+  .titleWrapper{
+    padding: 0.0375rem 0.3125rem 0.375rem 0.3125rem;
+  }
+  .title{
+    font-size: 0.7rem;
+    color: #555555;
+    text-align: center;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+</style>
